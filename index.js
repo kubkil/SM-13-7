6319a66823432292c2459781f1ad88810f57f853
Jakub Kilar <kuba.kilar@gmail.com>
2018-03-31 16:51:32 +0200

    finish app

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const fs = require('fs');
-const colors = require('colors');
-const StatMode = require('stat-mode');
 
-fs.readFile('./text.txt', 'utf-8', function (err, data) {
-  console.log('Dane przed zapisem!'.blue);
-  console.log(data);
-  fs.appendFile('./text.txt', '\nA tak wyglądają po zapisie!', function (err) {
+fs.readdir('./', 'utf-8', function (err, files) {
+  if (err) throw err;
+  console.log('Gotowe');
+  fs.writeFile('./folderContent.txt', files, function (err) {
     if (err) throw err;
-    console.log('Zapisano!'.blue);
-    fs.readFile('./text.txt', 'utf-8', function (err, data) {
-      console.log('Dane po zapisie'.blue)
-      console.log(data);
+    fs.readFile('./folderContent.txt', function (err, data) {
+      if (err) throw err;
+      console.log('Zawartość pliku to: ' + data);
     });
   });
 });
+
+// fs.readFile('./text.txt', 'utf-8', function (err, data) {
+//   console.log('Dane przed zapisem!'.blue);
+//   console.log(data);
+//   fs.appendFile('./text.txt', '\nA tak wyglądają po zapisie!', function (err) {
+//     if (err) throw err;
+//     console.log('Zapisano!'.blue);
+//     fs.readFile('./text.txt', 'utf-8', function (err, data) {
+//       console.log('Dane po zapisie'.blue)
+//       console.log(data);
+//     });
+//   });
+// });
