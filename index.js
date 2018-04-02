6cc5d22ee0fb9ae6c7963039afbacb0bb3decc26
Jakub Kilar <kuba.kilar@gmail.com>
2018-04-02 14:44:08 +0200

    finish function

--- index.js
+++ index.js
@@ -7,20 +7,7 @@ fs.readdir('./', 'utf-8', function (err, files) {
     if (err) throw err;
     fs.readFile('./folderContent.txt', function (err, data) {
       if (err) throw err;
-      console.log('Zawartość pliku to: ' + data);
+      console.log('Zawartość folderContent.txt to: ' + data);
     });
   });
 });
-
-// fs.readFile('./text.txt', 'utf-8', function (err, data) {
-//   console.log('Dane przed zapisem!'.blue);
-//   console.log(data);
-//   fs.appendFile('./text.txt', '\nA tak wyglądają po zapisie!', function (err) {
-//     if (err) throw err;
-//     console.log('Zapisano!'.blue);
-//     fs.readFile('./text.txt', 'utf-8', function (err, data) {
-//       console.log('Dane po zapisie'.blue)
-//       console.log(data);
-//     });
-//   });
-// });
